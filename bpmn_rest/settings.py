a78cd92a74e7204ca056ee0719caa86acd94ae18
MichaelN <nabiullin.m@numdes.com>
2024-01-10 19:27:29 +0300

    fix authentication

--- bpmn_rest/settings.py
+++ bpmn_rest/settings.py
@@ -130,3 +130,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+}
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
